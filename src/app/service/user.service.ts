87707265ef5b9d1e0b3e259fc40b933ba988a89c
Jorge Ivan Villalba Grandett <jivillalba@softcaribbean.com>
2023-04-02 18:25:32 -0500

    Se crean los enviroment y el servicio de coneccion con el back

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getById(id: number) {
+    return this.http.get(`${environment.url_api}/user/${id}`);
+  }
+
+  getAll() {
+    return this.http.get(`${environment.url_api}/user/list-users`);
+  }
+
+  createUser(data: any): Observable<any> {
+    return this.http.post(`${environment.url_api}/user/create-user`, data);
+  }
+
+  updateUser(id: number, data: any): Observable<any> {
+    return this.http.put(`${environment.url_api}/user/update-user/${id}`, data);
+  }
+
+  deleteUser(id: number, data: any): Observable<any> {
+    return this.http.delete(`${environment.url_api}/user/update-user/${id}`);
+  }
+}
